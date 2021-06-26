7b13cf656c67decab6915fd9bf221d2eaabbb7ea
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-06-26 20:08:16 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -216,11 +216,13 @@ if __name__ == "__main__":
     )
     app.add_handler(new_join_handler)
     #
+    """
     group_new_join_handler = MessageHandler(
         help_message_f,
         filters=filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members,
     )
     app.add_handler(group_new_join_handler)
+    """
     #
     call_back_button_handler = CallbackQueryHandler(button)
     app.add_handler(call_back_button_handler)
