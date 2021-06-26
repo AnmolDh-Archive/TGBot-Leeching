034ee6bdc024942aafbb2492c0f982f13a8f0f0a
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-06-26 19:38:26 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) Shrimadhav U K | Jigarvarma2005
 
 import logging
 
 import pyrogram
-from tobrot import AUTH_CHANNEL, LOGGER
+# the logging things
+from tobrot import AUTH_CHANNEL
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
 
 
 async def new_join_f(client, message):
@@ -13,18 +22,20 @@ async def new_join_f(client, message):
     if chat_type != "private":
         await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
         # leave chat
-        await client.leave_chat(chat_id=message.chat.id, delete=True)
+        await client.leave_chat(
+            chat_id=message.chat.id,
+            delete=True
+        )
     # delete all other messages, except for AUTH_CHANNEL
     await message.delete(revoke=True)
 
 
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you", quote=True)
-    # channel_id = str(AUTH_CHANNEL)[4:]
-    # message_id = 99
-    # display the /help
-
-    await message.reply_text(
-        """Use /help""",
-        disable_web_page_preview=True,
-    )
+    await message.reply_text("Hello \nI am Telegram Leech Robot! \nClick Below to know how to use me.", 
+                             reply_markup=InlineKeyboardMarkup(
+                                                               [
+                                                                  [
+                                                                   InlineKeyboardButton("Help", url=f"https://telegra.ph/Universal-Leecher-Help-Menu-03-18")
+                                                                  ]
+                                                               ]
+                            ), disable_web_page_preview=True, parse_mode="markdown")
